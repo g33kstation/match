3e08a93bfb596361e2f50ef7fc6a0e8a4ca9a602
Oumar FALL <oumarfall@hotmail.fr>
2015-06-03 13:49:35 +0000

    Server et templating minimal (ceci est un P.O.C.)
    Pleins d'optimisations à faire:
    * Chargement automatique des templates (filepath.Glob)
    * Clone() du template base.html
    * Gzip du flux HTTP
    * etc.

--- server.go
+++ server.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
+var tmpl map[string]*template.Template
+
+// init is run before the application starts serving.
+func init() {
+	if tmpl == nil {
+		tmpl = make(map[string]*template.Template)
+	}
+	//baseTmpl := template.Must(template.ParseFiles("templates/base.html"))
+	tmpl["home"] = template.Must(template.ParseFiles("templates/index.html", "templates/base.html"))
+	tmpl["register"] = template.Must(template.ParseFiles("templates/register.html", "templates/base.html"))
+}
+
+type Page struct {
+	Title   string
+	Content string
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Affichage des profils</h1>")
+	hm := Page{"Bienvenue", "Hello wrrrrld..."}
+	err := tmpl["home"].ExecuteTemplate(w, "base", hm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Fprintf(w, "%#v", tmpl["home"])
 }
 
 func registrationPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Formulaire inscription</h1>")
 }
